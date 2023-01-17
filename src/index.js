9291fd604d3435cf4522b24222d5493bbc6f636a
dolf <kolyakutylov@gmail.com>
2023-01-17 13:21:50 +0300

    task is solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const config = bracketsConfig.reduce((acc, pair) => {
+    return {
+      ...acc,
+      [pair[0]]: pair[1]
+    }
+  }, {})
+  const stack = []
+
+  for (let ch of str) {
+    if (ch === config[ch]) {
+      const similarCh = stack[stack.length - 1]
+
+      if (similarCh === ch) {
+        stack.pop()
+        continue
+      }
+    }
+    if (config[ch]) {
+      stack.push(config[ch])
+    } else {
+      const chFromStack = stack.pop()
+
+      if (ch !== chFromStack) {
+        return false
+      }
+    }
+  }
+
+  return stack.length === 0
+  // {
+  //  '(': ')',
+  //  '[': ']' 
+  // }
 }
